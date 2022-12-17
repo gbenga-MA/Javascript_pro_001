84ef156b17cc2d9141ed594244abc4a05178f849
gbenga-MA <michaeladeosun@ymail.com>
2022-12-17 10:41:03 +0100

    USING IF WITH IF OR ELSE CHAIN

---
+++ 001/If_and_IF_Or_ELSE_chain.js
@@ -0,0 +1,19 @@
+function fabricsSize(size){
+    if (size < 5){
+        return "Small";
+    }
+    else if (size < 10){
+        return "Medium";
+    }
+    else if (size < 15){
+        return " Large";
+    }
+    else if (size < 20){
+        return "Xtra Large";
+    }
+    else {
+        return "Huge";
+    }
+}
+fabricsSize(5);
+
