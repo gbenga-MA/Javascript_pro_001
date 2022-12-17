0b99986108ec7f2d63783004fbe0776374218d3e
gbenga-MA <michaeladeosun@ymail.com>
2022-12-17 11:18:36 +0100

    ADDING GREATER THAN SIGN

---
+++ 001/Adding_greater-than.js
@@ -0,0 +1,12 @@
+function testCoding(value){
+    if (value > 30){
+        result = "Greater than 30";
+    }
+    else {
+        result = "Less than 30";
+    }
+
+    return result;
+} 
+testCoding(35);
+
