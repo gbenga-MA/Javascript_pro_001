7968e34997711bc21648c6da526fe5e60966a5ae
gbenga-MA <michaeladeosun@ymail.com>
2022-12-17 11:14:45 +0100

    IF OR ELSE STATEMENT

---
+++ 001/Adding_IF_OR_ELSE_statement.js
@@ -0,0 +1,12 @@
+function testCoding(value){
+    if (value >= 100){
+        return "Wow! it's an HUNNID";
+    }
+     else if (value <= 50){
+    return "Ooops! almost an Hunnid";
+    }
+    else {
+        return "Sorry! you just missed an hunnid";
+    }
+}
+testCoding(120);
