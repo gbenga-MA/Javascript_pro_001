688b6213529190872d152058c05d031a9a721b47
gbenga-MA <michaeladeosun@ymail.com>
2023-05-08 15:26:00 +0100

    TERNARY CONDITIONAL OPERATION

---
+++ 001/Using_Ternary_Conditional_Statement.js
@@ -0,0 +1,5 @@
+/*Condition ? statement-if-true : statement-if-false;         */
+function testCoding(value){
+    return value > 10 ? "Absolute Value" : value < 5 ? "Perfect Match" : "Nearly Absolute";
+}
+testCoding(19);
