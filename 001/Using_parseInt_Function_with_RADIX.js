cb667fb67b3b15fecce30960e0aeb4e117356ac7
gbenga-MA <michaeladeosun@ymail.com>
2023-05-08 15:29:46 +0100

    parseInt Function with Radix

---
+++ 001/Using_parseInt_Function_with_RADIX.js
@@ -0,0 +1,7 @@
+/*A radix specifies the base of a number in a string; such as base2,base7,base8....Default base10
+*/
+
+function codeConversion(str){
+    return parseInt(str, 2);
+}
+codeConversion("10011");
